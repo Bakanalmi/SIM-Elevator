a796ac34acdb8a918d2849aa48261aaa8821a0d7
Bakanalmi <marc2almi@gmail.com>
2021-12-20 22:16:24 +0100

    refactor

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
+from event.Event import Event
 from util import parser
-import alterns as alt_strategy
-import express as xps_strategy
-import sys, random
+from event.Constants import *
+import sys
 
-values_path = './values.yaml'
-if len(sys.argv) > 1:
-    values_path = sys.argv[1]
+class Main:
 
+    values_path = './values.yaml'
+    if len(sys.argv) > 1:
+        values_path = sys.argv[1]
 
-def main():
-    print("Parsing values from", values_path)
-    values = parser.load_simulation_values(values_path)
-    simulation = values.get('simulation')
-    lapse = simulation.get('time')
-    strategy = simulation.get('strategy')
-    time = 3600 * (lapse.get('to') - lapse.get('from'))
-    random.seed(simulation.get('seed'))
+    def __init__(self):
+        self.simulationStart = Event(self, 0, EventType.SimulationStart, None)
 
-    if strategy <= 1:
+    def run(self):
+        print("Parsing values from", self.values_path)
+        values = parser.load_simulation_values(self.values_path)
+        simulation = values.get('simulation')
+        lapse = simulation.get('time')
+        time = 3600 * (lapse.get('to') - lapse.get('from'))
+
         print("Setting up alternating environment.")
         alterns, plot = alt_strategy.setup(simulation)
         print("Running alternating strategy")
         alterns.run(until=time)
-
-        plot.show()
-
-    if strategy < 1 or strategy == 2:
-        print("Setting up express environment.")
-        express, plot = xps_strategy.setup(simulation)
-        print("Running express strategy")
-        express.run(until=time)
 
         plot.show()
 
